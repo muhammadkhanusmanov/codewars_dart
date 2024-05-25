a21c5283311e65c5ffcba86bd40f971d80e7f50e
username <email>
2024-05-25 23:24:52 +0300

    .

---
+++ 8_kyu/task3.dart
@@ -0,0 +1,19 @@
+List<bool> flickSwitch(List<String> lst) {
+  List<bool> result = [];
+  bool a = true;
+  for (int i = 0; i < lst.length; i++) {
+    if (lst[i] == 'flick') {
+      result.add(!a);
+      a =!a;
+    }
+    else {
+      result.add(a);
+    }
+ }
+ return result;
+}
+
+
+void main() {
+  print(flickSwitch(['bicycle', 'jarmony', 'flick', 'sheep', 'flick']));
+}
